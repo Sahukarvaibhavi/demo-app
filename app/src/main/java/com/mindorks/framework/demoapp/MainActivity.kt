194360ef4ebb58441bc046931fafcf1a3d082134
Sahukarvaibhavi <vaibhavisahukar618@gmail.com>
2021-06-25 13:58:58 +0530

    added commit

--- app/src/main/java/com/mindorks/framework/demoapp/MainActivity.kt
+++ app/src/main/java/com/mindorks/framework/demoapp/MainActivity.kt
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 
+//A demo for github
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
